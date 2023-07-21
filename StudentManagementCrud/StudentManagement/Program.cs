b73f9e4a79ef8b7dc0113e103de4eac0c7da1f04
Pradyumn Patil <119255271+pradyumn29@users.noreply.github.com>
2023-07-21 23:03:03 +0530

    Tester

---
+++ StudentManagementCrud/StudentManagement/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DAL;
+using BOL;
+
+List<Student> students = DBManager.getAllStudents();
+
+foreach(var stud in students)
+{
+    Console.WriteLine(stud);
+}
+
+Student student =DBManager.getStudentById(1);
+Console.WriteLine(student);
+
+Student student=new Student(2,"sushant",23,"01/01/2020","QSpider",30000)
